09cb69922933723c599ae15d192d410e0303b23c
pkb-pmj <pkb.pmj@gmail.com>
2026-02-01 21:03:13 +0100

    🔧 use HTTPS in dev server to enable OPFS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import sqlocalPlugin from 'sqlocal/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), sqlocalPlugin()],
+	plugins: [sveltekit(), sqlocalPlugin(), basicSsl()],
+	server: {
+		https: true,
+	},
 });
