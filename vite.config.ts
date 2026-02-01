d04581ce46f00a74238d5f0e5696dc4137547db9
pkb-pmj <pkb.pmj@gmail.com>
2026-02-01 17:28:42 +0100

    🔧 add sqlocal plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import sqlocalPlugin from 'sqlocal/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), sqlocalPlugin()],
 });
