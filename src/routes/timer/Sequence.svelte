3d4ab1a30c23e88c9bcfab4201014cdc67e339c3
pkb-pmj <pkb.pmj@gmail.com>
2026-02-14 23:41:35 +0100

    💄 much better Sequence UI, still a lot of work ahead

--- src/routes/timer/Sequence.svelte
+++ src/routes/timer/Sequence.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import NodePicker from '$lib/components/NodePicker.svelte';
 	import { db, reactiveQuery } from '$lib/db';
-	import { formatTime } from '$lib/util';
+	import { formatDuration, formatTime, timeNow } from '$lib/util';
+	import { durationNow } from '$lib/util';
 
 	interface Props {
 		id: number;
@@ -20,7 +21,9 @@
 					'intervals.id',
 					'intervals.start_time',
 					'intervals.end_time',
+					'start_node.id as start_id',
 					'start_node.name as start_name',
+					'end_node.id as end_id',
 					'end_node.name as end_name',
 				])
 				.where('sequence_id', '=', id)
@@ -56,18 +59,112 @@
 			.where('end_time', 'is', null)
 			.execute();
 	}
+
+	type TimelineItem = {
+		time: number | null;
+		node_id: number | null;
+		node_name: string | null;
+		duration: number | null;
+		live: boolean;
+	};
+
+	let timeline = $derived.by(() => {
+		const items: TimelineItem[] = [];
+		const intervalsList = intervals;
+		for (const interval of intervalsList) {
+			const { start_id, start_name, start_time, end_id, end_name, end_time } = interval;
+			if (
+				!items.length ||
+				items.at(-1)?.time !== start_time ||
+				items.at(-1)?.node_id !== start_id
+			) {
+				items.push({
+					time: start_time,
+					node_id: start_id,
+					node_name: start_name,
+					duration: null,
+					live: false,
+				});
+			}
+
+			items.push({
+				time: end_time,
+				node_id: end_id,
+				node_name: end_name,
+				duration: end_time === null ? start_time : end_time - start_time,
+				live: end_time === null,
+			});
+		}
+		return items;
+	});
+
+	$inspect(timeline);
 </script>
 
-<ul>
-	{#each $intervals as interval (interval.id)}
-		<li>
-			<span>
-				{interval.start_name}
-				{formatTime(interval.start_time)} -> {interval.end_name}
-				{formatTime(interval.end_time)}
-			</span>
-		</li>
-	{/each}
-</ul>
-<button onclick={cancelLastInterval}>Finish here</button>
-<NodePicker onPicked={moveToNextInterval} />
+<div class="container">
+	<ul>
+		{#each timeline as { duration, live, time, node_name }, i}
+			{#if duration}
+				<li class="edge" class:live>
+					<span class="time">
+						{formatDuration(live ? duration : durationNow(duration))}
+					</span>
+					<span class="divider">|</span>
+				</li>
+			{/if}
+			<li class="node" class:live>
+				<span class="time">{formatTime(time ?? timeNow())}</span>
+				<span class="divider">O</span>
+				{node_name}
+			</li>
+		{/each}
+	</ul>
+	<button class="finish" onclick={cancelLastInterval}>Finish here</button>
+	<NodePicker onPicked={moveToNextInterval} />
+</div>
+
+<style>
+	.container {
+		margin: 0.5rem;
+		padding: 0.5rem;
+		border-radius: 1rem;
+		border: 1px solid #ccc;
+	}
+
+	ul {
+		padding: 0.5rem;
+	}
+
+	li {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	span.time {
+		flex: 0;
+		flex-basis: 4rem;
+		font-size: 0.75rem;
+		text-align: right;
+	}
+
+	.live {
+		color: #08f;
+	}
+
+	.live span.time {
+		font-style: italic;
+	}
+
+	span.divider {
+		flex: 0;
+		flex-basis: 1.5rem;
+		text-align: center;
+	}
+
+	button.finish {
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+		border: 1px solid #ccc;
+	}
+</style>
