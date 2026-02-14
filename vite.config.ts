eab5147e931051041ade53307b37e350be14913f
pkb-pmj <pkb.pmj@gmail.com>
2026-02-14 13:34:25 +0100

    🔧 setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import sqlocalPlugin from 'sqlocal/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), sqlocalPlugin(), basicSsl()],
+	plugins: [tailwindcss(), sveltekit(), sqlocalPlugin(), basicSsl()],
 	server: {
 		https: true,
 	},
