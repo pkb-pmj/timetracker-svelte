36f2ab8a12bdaa81a9c165b471115fc228d28188
pkb-pmj <pkb.pmj@gmail.com>
2026-02-15 23:41:12 +0100

    💄 much better timeline using CSS grid

--- src/routes/timer/Sequence.svelte
+++ src/routes/timer/Sequence.svelte
@@ -141,15 +141,17 @@
 		<span class="time">
 			{formatDuration(duration)}
 		</span>
-		<span class="divider">|</span>
+		<div class="line"></div>
 	</li>
 {/snippet}
 
 {#snippet timelineNode(time: number, name: string | null, live: boolean = false)}
 	<li class="node" class:live>
 		<span class="time">{formatTime(time)}</span>
-		<span class="divider">O</span>
-		{name}
+		<div class="line top"></div>
+		<div class="line bottom"></div>
+		<div class="circle"></div>
+		<span class="description">{name}</span>
 	</li>
 {/snippet}
 
@@ -181,34 +183,94 @@
 	}
 
 	ul {
-		padding: 0.5rem;
+		display: grid;
+		/* 0.8rem instead of min-content, handles padding for durations */
+		/* the other option would be min-content + manual padding in duration, */
+		/* both are equally manual and interdependent */
+		grid-template-columns: min-content 0.8rem 1fr;
 	}
 
+	/* inherit columns from <ul>, contain children in one row without explicit grid-row */
 	li {
-		display: flex;
-		flex-direction: row;
+		grid-column: 1 / -1;
+		display: grid;
+		grid-template-columns: subgrid;
 		align-items: center;
 	}
 
-	span.time {
-		flex: 0;
-		flex-basis: 4rem;
+	.time {
+		grid-column: 1;
+		grid-row: 1;
 		font-size: 0.75rem;
 		text-align: right;
+		/* equal width digits */
+		font-variant-numeric: tabular-nums;
 	}
 
+	.node .time {
+		/* move timestamp further left, so timestamp-circle and duration-line distances are equal */
+		/* margin instead of padding so width is independent */
+		margin-right: 0.25rem;
+		/* 8 characters - hh:mm:ss TODO: different time formats */
+		width: 8ch;
+	}
+
+	.line {
+		grid-row: 1;
+		grid-column: 2;
+		justify-self: center;
+		align-self: stretch;
+		/* yes, it does center itself correctly */
+		border-left: 1.5px solid black;
+		z-index: -1;
+	}
+
+	.line.top {
+		height: 50%;
+		align-self: start;
+	}
+
+	.line.bottom {
+		height: 50%;
+		align-self: end;
+	}
+
+	/* ensure line doesn't stick out beyond first or last node */
+	li.node:first-child .line.top,
+	li.node:last-child .line.bottom {
+		display: none;
+	}
+
+	.circle {
+		grid-column: 2;
+		grid-row: 1;
+		place-self: center;
+		border-radius: 50%;
+		border: 1.5px solid black;
+		background-color: white;
+		width: 0.6rem;
+		height: 0.6rem;
+	}
+
+	/* color active interval differently */
 	.live {
 		color: #08f;
-	}
-
-	.live span.time {
 		font-style: italic;
 	}
 
-	span.divider {
-		flex: 0;
-		flex-basis: 1.5rem;
-		text-align: center;
+	.live .circle,
+	.live .line,
+	/* including the bottom line segment of previous node */
+	li.node:has(+ .live) .line.bottom {
+		border-color: #08f;
+	}
+
+	.description {
+		grid-column: 3;
+	}
+
+	.node .description {
+		margin-left: 0.25rem;
 	}
 
 	button.finish {
