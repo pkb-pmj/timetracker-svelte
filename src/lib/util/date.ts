e3905f6b28eb06c616905c79718b7b959f0840aa
pkb-pmj <pkb.pmj@gmail.com>
2026-02-25 16:22:27 +0100

    💄 better, more compact time/duration formatting

--- src/lib/util/date.ts
+++ src/lib/util/date.ts
@@ -3,14 +3,18 @@ export function formatTime(timestamp: Date | number | null) {
 	if (typeof timestamp === 'number') {
 		timestamp = new Date(timestamp);
 	}
-	return timestamp.toLocaleTimeString();
+	const hours = timestamp.getHours().toString().padStart(2, '0');
+	const minutes = timestamp.getMinutes().toString().padStart(2, '0');
+	return `${hours}:${minutes}`;
 }
 
 export function formatDuration(millis: number) {
 	let seconds = Math.floor(millis / 1000) % 60;
 	let minutes = Math.floor(millis / (60 * 1000)) % 60;
 	let hours = Math.floor(millis / (60 * 60 * 1000)) % 24;
-	if (hours) return `${hours}h${minutes.toString().padStart(2, '0')}m`;
-	else if (minutes) return `${minutes}m${seconds.toString().padStart(2, '0')}s`;
+	if (hours >= 10) return `${hours}h`;
+	else if (hours) return `${hours}h${minutes.toString()}m`;
+	else if (minutes >= 10) return `${minutes}m`;
+	else if (minutes) return `${minutes}m${seconds.toString()}s`;
 	else return `${seconds}s`;
 }
